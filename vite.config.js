46e093f5f36c0c15f637078690c8baff6341cb6c
Wujidadi <wujidadi@gmail.com>
2023-04-30 18:19:36 +0800

    Set vite.config.js, fix the port of vite dev for mapping to outside the container

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,25 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        svgLoader(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 49657,
+    },
+    resolve: {
+        alias: {
+            '@': '/resources',
+            'vue': 'vue/dist/vue.esm-bundler.js',
+            'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+        },
+    },
 });
